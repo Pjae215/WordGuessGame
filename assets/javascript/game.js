ba210dfb2faace967c31726dfedd9be7c2b0d79b
Pjae215 <lala2462002@hotmail.com>
2019-06-07 20:47:10 -0400

    last hurrah

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,23 @@
 
-    // letter and word choices
-    var letterchoice = ["abcdefghijklmnopqrstuvwxyz"].split("");
-    
-    var wordList = ["britney", "selena", "bob dylan", "john legend", "michael jackson", "pink", "chris brown", "rihanna", "adele"];
+// letter and word choices
 
-    // We start the game with a score of 0.
-    var score = 0;
-    // Variable to hold the index of current question.
-    var letterIndex = 0;
+var letterchoice = ["abcdefghijklmnopqrstuvwxyz"].split("");
+    
+var wordList = ["britney", "selena", "bob dylan", "john legend", "michael jackson", "pink", "chris brown", "rihanna", "adele"];
+
+// User starts with a score of 0.
+ var score = 0;
+
+// Assigning name to user entry and converting it to match list format
+var usertext = ".guessLetter"
+var userGuessLower = usertext.toLowerCase();
+
+
+document.getElementById("button").addEventListener("click", function(){ document.getElementById("entry").innerHTML = "usertext"
+});
+
 
     /* User enters one letter, if the letter is in the first word then the user gets a prompt that says its a match and score +10 , if not then say guess again and score -5, need to display letters that are chosen and matched or unmatched,need to tally score and need to reset all and repeat process with next word */
 
-    // converts user guess to match array
-    var userGuessLower = userGuess.toLowerCase();
-    
+
   
